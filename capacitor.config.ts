a0be6f031b9a997df723d764b8a2c72fa4f9c912
Zira <ziradeveloper@gmail.com>
2025-09-06 10:50:06 +0530

    android package name fixed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.ziradeveloper.studybuddy', // ✅ unique ID
   appName: 'study-buddy',
   webDir: 'dist'
 };
